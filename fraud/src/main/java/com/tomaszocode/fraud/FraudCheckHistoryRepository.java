8d7de4c0ca4e62088d9c15d5e551dc24594b6ad6
Tomasz-asus <81529362+Tomasz-asus@users.noreply.github.com>
2022-10-19 21:33:03 +0200

    add docker hub

--- fraud/src/main/java/com/tomaszocode/fraud/FraudCheckHistoryRepository.java
+++ fraud/src/main/java/com/tomaszocode/fraud/FraudCheckHistoryRepository.java
@@ -1,7 +1,10 @@
 package com.tomaszocode.fraud;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FraudCheckHistoryRepository
-        extends JpaRepository<FraudCheckHistory, Integer> {
+        extends JpaRepository<FraudCheckHistory, Integer>, JpaSpecificationExecutor<FraudCheckHistory> {
 }
